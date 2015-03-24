141c7cf55e88344ca8f9cc564b9f18438a95db44
jjjcoutu <jjjcoutu@gmail.com>
2015-03-24 17:24:39 -0400

    segmented featureset in main, got class means and covariances

--- main.m
+++ main.m
@@ -10,6 +10,40 @@ load ./resources/feat.mat
 feature = f32;
 featureTest = f32t;
 
+%preallocate for speed
+means = zeros(2,10);
+testMeans = means;
+variances = zeros(2,20);
+testVariances = variances;
+datapoints = feature(1:2,1:160);
+testDatapoints = feature(1:3,1:160); % using a different format so we have class info bound to our test data.
+
+% 10 images. These numbers, coincidentally, also correspond to what class
+% the data point belongs to.
+for i=1:10
+        count = 0;
+        % 16 blocks per image
+        %1-16, 17-32
+        %lower bound: (16*i-15)
+        %upper bound: (16*i)
+        
+        data = feature(1:2,16*i-15:16*i);              
+        testdata = featureTest(1:2,16*i-15:16*i);      
+        %these should each be a 2x16 matrix.
+        
+        %our mean/covariance thing takes a COLUMN of ROW VECTORS. rough.
+        [m,S] = getMeanCovar(data');
+        means(1:2,i) = m';
+        variances(1:2,i*2-1:i*2) = S;
+        
+        [m,S] = getMeanCovar(testdata');
+        testMeans(1:2,i) = m';
+        testVariances(1:2,i*2-1:i*2) = S;   
+        
+        clear data testdata
+end
+        
+
 
 
 
